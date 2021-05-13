d7d796948f38c84e05268c6e9d5c2a4ed64c1393
Gauri Tanwar <gauritanwar2507@gmail.com>
2021-05-13 14:35:15 +0530

    update done!!

--- app/src/main/java/com/streamliners/counterapp/MainActivity.java
+++ app/src/main/java/com/streamliners/counterapp/MainActivity.java
@@ -1,40 +1,108 @@
 package com.streamliners.counterapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.streamliners.counterapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private int qty = 0;
     ActivityMainBinding b;
 
+    private String COUNT_VALUE = "countValue";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        // Initializing the binding
         b = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
 
-        setUpEventHandlers();
+        //Handel click events
+        eventHandler();
+
+        handelConfigurationChanges(savedInstanceState);
     }
 
-    private void setUpEventHandlers() {
-        b.incQty.setOnClickListener(new View.OnClickListener() {
+    /**
+     * Handle data on configuration change
+     * @param savedInstanceState To get data from bundle
+     */
+    private void handelConfigurationChanges(Bundle savedInstanceState) {
+        //Check savedInstanceState not null
+        if (savedInstanceState != null) {
+            qty = savedInstanceState.getInt(COUNT_VALUE);
+        } else {
+            //Get data from sharedPreference
+            qty = getPreferences(MODE_PRIVATE)
+                    .getInt(COUNT_VALUE, 0);
+
+        }
+        b.qty.setText(qty + "");
+    }
+
+    /**
+     * Trigger Event handlers to listen the actions
+     */
+    private void eventHandler() {
+        //click listener on Decrease button
+        b.decBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                incQty();
+            public void onClick(View v) {
+                //To decrease quantity
+                decreaseQuantity();
             }
         });
 
-        b.incQty.setOnClickListener(new View.OnClickListener() {
+        //click listener on Increase button
+        b.incBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                decQty();
+            public void onClick(View v) {
+                //To increase quantity
+                increaseQuantity();
             }
         });
     }
 
+    /**
+     * Increase the quantity
+     * update TextView
+     */
+    private void increaseQuantity() {
+        //update quantity and textView
+        b.qty.setText(++qty + "");
+    }
 
+    /**
+     * Decrease the quantity
+     * update TextView
+     */
+    private void decreaseQuantity() {
+
+        //check quantity
+        if (qty == 0) {
+            //show toast message
+            Toast.makeText(this, "Quantity is already 0", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //update quantity and textView
+        b.qty.setText(--qty + "");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(COUNT_VALUE, qty);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferences(MODE_PRIVATE).edit().putInt(COUNT_VALUE, qty).apply();
+    }
 }
